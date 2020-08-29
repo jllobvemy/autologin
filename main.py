96b7c2b56662189db8b70e147a6f4b30a1e0a799
jllobvemy <jllobemy@gmail.com>
2020-08-29 12:05:05 +0800

    operators add

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ import os
 
 c = campusNetwork()
 if not os.path.exists('./setting.json'):
-    user = {'username': '', 'passwd': ''}
+    user = {'username': '', 'passwd': '', 'operators': ''}
     with open('./setting.json', 'w') as f:
         json.dump(user, f, indent=4)
 with open('./setting.json', 'r') as f:
     user = json.load(f)
-    c.login(user['username'], user['passwd'], operators.ChinaNet)
+    if user['operators'] == 'ChinaNet':
+        c.login(user['username'], user['passwd'], operators.ChinaNet)
+    elif user['operators'] == 'CMCC':
+        c.login(user['username'], user['passwd'], operators.CMCC)
+    elif user['operators'] == 'School':
+        c.login(user['username'], user['passwd'], operators.School)
     c.showtoast()
